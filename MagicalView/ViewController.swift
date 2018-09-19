22041899ff6a84705a4e023c77715db924c8776b
Sindhura <sindhura@comp037.local>
2018-09-19 15:45:40 +0530

    Initial Setup

--- MagicalView/ViewController.swift
+++ MagicalView/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let fantasticView = FantasticViews(frame: self.view.bounds)
+        self.view.addSubview(fantasticView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
